d983669b3aa096181f04d5cea658d5bf7d859493
Mario Arnautou <mario.arnautou@gmail.com>
2023-01-12 16:11:11 +0100

    fix: security issue by upgrading jwt (#32)

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,19 +1,14 @@
 import { AzureJwtError, invalidateCache, verifyAzureToken } from '../src';
-import { SinonStub, stub } from 'sinon';
-import jsonwebtoken from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import nock from 'nock';
 import discoveryResponse from './mocks/discovery-response.json';
+jest.mock('jsonwebtoken');
 
 const mocks = {
   options: { audience: 'AUD', issuer: 'ISS' },
   token: '$TOKEN',
   decoded: { foo: 'bar' },
   discoveryResponse,
-};
-
-let stubs: {
-  verify: SinonStub;
-  decode: SinonStub;
 };
 
 const discovery = {
@@ -27,73 +22,116 @@ const key = `-----BEGIN CERTIFICATE-----\n${
 }\n-----END CERTIFICATE-----`;
 
 describe('The verify AzureAD JWT method', () => {
+  let stubs: Record<string, jest.SpyInstance>;
   beforeEach(() => {
     stubs = {
-      verify: stub(jsonwebtoken, 'verify'),
-      decode: stub(jsonwebtoken, 'decode'),
+      decode: jest.spyOn(jwt, 'decode'),
+      verify: jest.spyOn(jwt, 'verify'),
     };
   });
   afterEach(() => {
     invalidateCache();
-    stubs.verify.restore();
-    stubs.decode.restore();
+    stubs.verify.mockRestore();
+    stubs.decode.mockRestore();
   });
   it('should decode token using public key provided by microsoft with the correct options', async () => {
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     expect(await verifyAzureToken(mocks.token, mocks.options)).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(1);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(1);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should use key from cache if caching is activated and cache has not expired [given TTL]', async () => {
     const options = { ...mocks.options, useCache: true, cacheTtl: 200 };
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
     nock(discovery.host).get(discovery.endpoint).replyWithError('Should not have been called !');
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     // First call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
     // Second call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(2);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(2);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should use key from cache if caching is activated and cache has not expired [default TTL]', async () => {
     const options = { ...mocks.options };
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
     nock(discovery.host).get(discovery.endpoint).replyWithError('Should not have been called !');
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     // First call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
     // Second call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(2);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(2);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should fetch key again if caching is activated and cache has expired [given TTL]', async () => {
     const options = { ...mocks.options, useCache: true, cacheTtl: 100 };
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     // First call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
     await new Promise<void>((resolve) => setTimeout(() => resolve(), 200));
     // Second call
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(2);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(2);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should fetch key again if caching is deactivated', async () => {
     const options = { ...mocks.options, useCache: false, cacheTtl: 100 };
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     // First call
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
     // Second call
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
     expect(await verifyAzureToken(mocks.token, options)).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(2);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(2);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should use provided discovery URL if given', async () => {
     const customUri = 'https://whatevr.com/';
     nock(customUri).get('/').reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     expect(
       await verifyAzureToken(mocks.token, {
         ...mocks.options,
@@ -101,14 +139,21 @@ describe('The verify AzureAD JWT method', () => {
         useCache: false,
       }),
     ).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(1);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(1);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should throw on 4xx when discovering keys', async () => {
     nock(discovery.host).get(discovery.endpoint).reply(400, 'Bad Request');
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
     try {
       await verifyAzureToken(mocks.token, { ...mocks.options, useCache: false });
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('ErrorFetchingKeys');
     }
   });
@@ -116,23 +161,41 @@ describe('The verify AzureAD JWT method', () => {
     nock(discovery.host).get(discovery.endpoint).reply(503, 'Service Unavailable');
     nock(discovery.host).get(discovery.endpoint).reply(502, 'Bad Gateway');
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     expect(await verifyAzureToken(mocks.token, { ...mocks.options, useCache: false })).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(1);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(1);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should retry on network failure', async () => {
     nock(discovery.host).get(discovery.endpoint).replyWithError('Network Failure :S');
     nock(discovery.host).get(discovery.endpoint).reply(503, 'Service Unavailable');
     nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, mocks.options).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     expect(await verifyAzureToken(mocks.token, { ...mocks.options, useCache: false })).toBe(mocks.decoded);
+    expect(stubs.decode).toHaveBeenCalledTimes(1);
+    expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+    expect(stubs.verify).toHaveBeenCalledTimes(1);
+    expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+    expect(stubs.verify.mock.calls[0][1]).toEqual(key);
+    expect(stubs.verify.mock.calls[0][2]).toEqual(mocks.options);
   });
   it('should retry once on 5xx if retry option is set to 1', async () => {
     nock(discovery.host).get(discovery.endpoint).reply(503, 'Service Unavailable');
     nock(discovery.host).get(discovery.endpoint).reply(502, 'Bad Gateway');
-    stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-    stubs.verify.withArgs(mocks.token, key, { ...mocks.options, useCache: false }).yields(null, mocks.decoded);
+    stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+    stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+      callback(null, mocks.decoded);
+    });
     try {
       await verifyAzureToken(mocks.token, {
         ...mocks.options,
@@ -140,6 +203,8 @@ describe('The verify AzureAD JWT method', () => {
         useCache: false,
       });
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('ErrorFetchingKeys');
     }
   });
@@ -159,9 +224,11 @@ describe('The verify AzureAD JWT method', () => {
   });
   it('should throw MissingKeyID if token header has no key ID', async () => {
     try {
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: undefined } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: undefined } }));
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('MissingKeyID');
     }
   });
@@ -170,9 +237,11 @@ describe('The verify AzureAD JWT method', () => {
       nock(discovery.host).get(discovery.endpoint).reply(503, 'Service Unavailable');
       nock(discovery.host).get(discovery.endpoint).reply(503, 'Service Unavailable');
       nock(discovery.host).get(discovery.endpoint).reply(502, 'Bad Gateway');
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('ErrorFetchingKeys');
     }
   });
@@ -181,45 +250,60 @@ describe('The verify AzureAD JWT method', () => {
       nock(discovery.host).get(discovery.endpoint).replyWithError('Network Failure :S');
       nock(discovery.host).get(discovery.endpoint).replyWithError('Network Failure :S');
       nock(discovery.host).get(discovery.endpoint).replyWithError('Network Failure :S');
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('ErrorFetchingKeys');
     }
   });
   it('should throw InvalidDiscoveryResponse if the discovery response is invalid', async () => {
     try {
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: 'non-existing-key' } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: 'non-existing-key' } }));
       await verifyAzureToken(mocks.token, { discoveryUrl: 'https://www.google.com/' });
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('InvalidDiscoveryResponse');
     }
   });
   it('should throw InvalidDiscoveryResponse if the discovery response is invalid', async () => {
     try {
       nock(discovery.host).get(discovery.endpoint).reply(200, { foo: 'bar' });
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: 'non-existing-key' } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: 'non-existing-key' } }));
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('InvalidDiscoveryResponse');
     }
   });
   it('should throw NotMatchingKey if there is no matching in microsoft response', async () => {
     try {
       nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: 'non-existing-key' } });
+      stubs.decode.mockImplementation(() => ({ header: { kid: 'non-existing-key' } }));
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
       expect((e as AzureJwtError).code).toBe('NotMatchingKey');
     }
   });
   it('should throw JsonWebTokenError if token cannot be decoded', async () => {
     try {
       nock(discovery.host).get(discovery.endpoint).reply(200, mocks.discoveryResponse);
-      stubs.decode.withArgs(mocks.token, { complete: true }).returns({ header: { kid: discovery.keyid } });
-      stubs.verify.withArgs(mocks.token, key).yields(new Error('Token expired'), null);
+      stubs.decode.mockImplementation(() => ({ header: { kid: discovery.keyid } }));
+      stubs.verify.mockImplementation((_token, _key, _options, callback) => {
+        callback(new Error('Token expired'));
+      });
       await verifyAzureToken(mocks.token);
     } catch (e) {
+      expect(stubs.decode).toHaveBeenCalledTimes(1);
+      expect(stubs.decode).toHaveBeenCalledWith(mocks.token, { complete: true });
+      expect(stubs.verify).toHaveBeenCalledTimes(1);
+      expect(stubs.verify.mock.calls[0][0]).toBe(mocks.token);
+      expect(stubs.verify.mock.calls[0][1]).toEqual(key);
       expect((e as AzureJwtError).code).toBe('JsonWebTokenError');
       expect((e as AzureJwtError).message).toBe('Token expired');
     }
